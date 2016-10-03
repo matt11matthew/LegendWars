22923a825dc920801b74bab4269520e6d38473e1
Matthew <matt11matthew@gmail.com>
2016-10-03 16:50:25 -0400

    config

---
+++ bans/src/main/java/com/matthewedevelopment/bans/LegendWarsBans.java
@@ -0,0 +1,13 @@
+package com.matthewedevelopment.bans;
+/* 
+ * Copyright © 2016 Matthew E Development - All Rights Reserved
+ * You may NOT use, distribute and modify this code.
+ */
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * Created by Matthew E on 10/3/2016 at 4:32 PM.
+ */
+public class LegendWarsBans extends Plugin {
+}
